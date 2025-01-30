bc578a2d88f69cd100f37a14ed7b935728d6c25c
zhao <2834226509@qq.com>
2025-01-30 14:50:49 +0800

    feat: 初始化自习室管理平台项目
    - 更新项目结构和命名- 添加 Spring Boot、MySQL、Redis、Spring Security、JWT 等依赖
    - 配置 Maven构建插件
    - 添加 Lombok 和 MyBatis 依赖- 更新 Feiyuan03Application 类

--- src/main/java/com/example/feiyuan03/Feiyuan03Application.java
+++ src/main/java/com/example/feiyuan03/Feiyuan03Application.java
@@ -3,6 +3,9 @@ package com.example.feiyuan03;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 28342
+ */
 @SpringBootApplication
 public class Feiyuan03Application {
 
